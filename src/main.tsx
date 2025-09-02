cbacd87ee7d18e394ab384380b569175434f44f6
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2025-09-02 13:35:34 -0400

    feat: i react-md-editor , tailwind typography. config css and integration on main an index

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App.tsx";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
 import "./index.css";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
